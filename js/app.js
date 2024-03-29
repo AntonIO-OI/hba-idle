9689a442ec13b674051cba30eeb5faa60b0eff40
AntonIO-OI <anton.bur1337@gmail.com>
2024-03-29 14:04:34 +0200

    feat: add catapult animation

--- js/app.js
+++ js/app.js
@@ -2,11 +2,22 @@ let ammoElement = document.getElementById("ammo");
 let pointsElement = document.getElementById("points");
 let gameField = document.getElementById("background");
 let copyBtn = document.getElementById("copy");
+let catapult = document.getElementById("catapult");
 
 let ammo = parseInt(localStorage.getItem("ammo")) || 0;
 let points = parseInt(localStorage.getItem("points")) || 0;
 let lastAmmoUpdate =
   parseInt(localStorage.getItem("lastAmmoUpdate")) || Date.now();
+let max_ammo = 5000;
+
+currentTime = Date.now();
+ammo = 0;
+ammoElement.textContent = `+${ammo}`;
+localStorage.setItem("ammo", ammo);
+localStorage.setItem("lastAmmoUpdate", currentTime);
+
+let points_per_tap = 5;
+let ammo_points = 5;
 
 console.log(ammo, points, lastAmmoUpdate);
 
@@ -14,11 +25,12 @@ pointsElement.textContent = points;
 ammoElement.textContent = `+${ammo}`;
 
 function updatePoints() {
-  if (ammo <= 0) return;
-  ammo--;
+  if (ammo - ammo_points < 0) return;
+  ammo -= ammo_points;
   ammoElement.textContent = `+${ammo}`;
-  points += 12;
+  points += points_per_tap;
   pointsElement.textContent = points;
+  catapultAnimate();
 
   localStorage.setItem("ammo", ammo);
   localStorage.setItem("points", points);
@@ -26,7 +38,7 @@ function updatePoints() {
 }
 
 function updateAmmo() {
-  ammo++;
+  ammo += ammo_points;
   ammoElement.textContent = `+${ammo}`;
 
   localStorage.setItem("ammo", ammo);
@@ -47,10 +59,18 @@ function initialAmmoUpdate() {
   let timeDifference = currentTime - lastAmmoUpdate;
 
   let ammoIncrement = Math.floor(timeDifference / 1000);
-  ammo += ammoIncrement;
+  if (ammo + ammoIncrement <= max_ammo) ammo += ammoIncrement * ammo_points;
+  else ammo = max_ammo;
   ammoElement.textContent = `+${ammo}`;
   localStorage.setItem("ammo", ammo);
   localStorage.setItem("lastAmmoUpdate", currentTime);
+}
+
+function catapultAnimate() {
+  catapult.classList.add("active");
+  setTimeout(function () {
+    catapult.classList.remove("active");
+  }, 400);
 }
 
 initialAmmoUpdate();
